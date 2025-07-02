25ede50bddde7843655b85135381c9a47d436075
Guillaumepln <74543855+Guillaumepln@users.noreply.github.com>
2025-07-02 19:08:59 +0200

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -8,5 +8,5 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id = "9f536dd9-9986-4eb8-a73d-7c9374b6d408"
+  subscription_id = "9f536dd9-9986-4eb8-a73d-7c9374b6d408" # Remplacer l'id par le vôtre (trouvé sur azure)
 }
